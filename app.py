99545ecc27a547c1e7f0b8613d326ae5f912512d
Stephan Griesel <sgriesel@gmail.com>
2023-03-08 06:40:01 +0100

    read database

--- app.py
+++ app.py
@@ -2,49 +2,32 @@ from dotenv import load_dotenv
 load_dotenv()
 
 import os 
+import requests
 
 # Get environment details
 # print(os.environ)
 
 token = os.environ.get("NOTION_TOKEN")
-token = os.environ.get("DATABASE_ID")
-
-print(os.environ.get("NOTION_TOKEN"))
-print(os.environ.get("DATABASE_ID"))
-
-import requests
-from datetime import datetime, timezone
-
-DATABASE_ID = "8ed35ea0fd1c4019a453871faef4cceb"
+databaseId = os.environ.get("DATABASE_ID")
 
 headers = {
-  "Authorization": "Bearer" + "NOTION_TOKEN",
-  "Content-Type": "application/json",
+  "Authorization": "Bearer " + token,
   "Notion-Version": "2022-06-28"
 }
 
-def get_pages():
-  url = f"https://api.notion.com/v1/databases/{DATABASE_ID}/query"
+print(token)
+print(databaseId)
 
-  payload = {"page_size": 100}
-  response = requests.post(url, json=payload, headers=headers)
+def readDatabase(databaseId, headers):
+  readUrl = f'https://api.notion.com/v1/databases/{databaseId}'
 
-  data = response.json()
+  res = requests.request("GET", readUrl, headers=headers)
+  print(res.status_code)
 
-  # Comment out to dump all data to a file
-  import json
-  with open('db.json', 'w', encoding='utf8') as f:
-    json.dump(data, f, ensure_ascii=False, indent=4)
+def createPage():
+  pass
 
-  results = data("results")
-  return results
+def updatePage():
+  pass
 
-pages = get_pages()
-for page in pages:
-  page_id = page["id"]
-  props = page["properties"]
-  url = props["URL"]["title"][0]["text"]["content"]
-  title = props["Title"]["rich_text"][0]["text"]["content"]
-  published = props["Published"]["date"]["start"]
-  published = datetime.fromisoformat(published)
-  print(url, title, published)
+readDatabase(databaseId,headers)
