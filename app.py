20bd43e05657d75ba117310de17d82d154118017
Stephan Griesel <sgriesel@gmail.com>
2023-03-16 06:39:29 +0100

    update fixes, test & finalise

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
+import os
+import json
+import requests
 from dotenv import load_dotenv
 load_dotenv()
 
-import os 
-import requests
-import json
 
 # Get environment details
 # print(os.environ)
@@ -12,94 +12,107 @@ token = os.environ.get("NOTION_TOKEN")
 databaseId = os.environ.get("DATABASE_ID")
 
 headers = {
-  "Authorization": "Bearer " + token,
-  "Content-Type": "application/json",
-  "Notion-Version": "2022-06-28"
+    "Authorization": "Bearer " + token,
+    "Content-Type": "application/json",
+    "Notion-Version": "2022-06-28"
 }
 
-print(token)
-print(databaseId)
+print("token test: ", token)
+print("database id: ", databaseId)
+
 
 def readDatabase(databaseId, headers):
-  readUrl = f'https://api.notion.com/v1/databases/{databaseId}/query'
+    readUrl = f'https://api.notion.com/v1/databases/{databaseId}/query'
 
-  res = requests.request("POST", readUrl, headers=headers)
+    res = requests.request("POST", readUrl, headers=headers)
 
-  # test data
-  data = res.json()
-  print(res.status_code)
-  # print(res.text)
+    # test data
+    data = res.json()
+    print("Read Database Status Code", res.status_code)
+    print("Read Database Response Text", res.text)
 
-  # dump data in file
-  with open('./db.json', 'w', encoding='utf8') as f:
-    json.dump(data, f, ensure_ascii=False)
+    # dump data in file
+    with open('./db.json', 'w', encoding='utf8') as f:
+        json.dump(data, f, ensure_ascii=False)
 
-def createPage(databaseId,headers):
-  
-  createUrl = 'https://api.notion.com/v1/pages'
 
-  newPageData = {
-    "parent": {"database_id": databaseId},
-    "properties": {
-      "Name": {
-        "title": [
-          {
-            "text": {
-              "content": "Hello One"
+readDatabase(databaseId, headers)
+
+
+def createPage(databaseId, headers):
+
+    createUrl = 'https://api.notion.com/v1/pages'
+
+    newPageData = {
+        "parent": {"database_id": databaseId},
+        "properties": {
+            "Name": {
+                "title": [
+                  {
+                      "text": {
+                          "content": "Hello One"
+                      }
+                  }
+                ]
+
+            },
+            "Value": {
+                "rich_text": [
+                    {
+                        "text": {
+                            "content": "This is one"
+                        }
+                    }
+                ]
+
+            },
+            "Status": {
+                "rich_text": [
+                    {
+                        "text": {
+                            "content": "Active"
+                        }
+                    }
+                ]
             }
-          }
-        ]
-
-      },
-      "Value": {
-        "rich_text": [
-          {
-            "text": {
-              "content": "This is one"
-            }
-          }
-        ]
-
-      },
-      "Status": {
-        "rich_text": [
-          {
-            "text": {
-              "content": "Active"
-            }
-          }
-        ]
-      }
+        }
     }
-  }
 
-  data = json.dumps(newPageData)
+    data = json.dumps(newPageData)
 
-  res = requests.request("POST", createUrl, headers=headers, data=data)
+    res = requests.request("POST", createUrl, headers=headers, data=data)
 
-  print(res.status_code)
-  print(res.text)
+    print("Create Page Status Code", res.status_code)
+    print("Create Page Response Text", res.text)
+
+
+createPage(databaseId, headers)
+
 
 def updatePage(pageId, headers):
-  updateUrl = f"https://api.notion.com/v1/pages/{pageId}"
+    updateUrl = f"https://api.notion.com/v1/pages/{pageId}"
 
-  updateData = {
-    "properties": {
-      "Value": {
-        "rich_text":[
-          {
-            "text":{
-              "content":"Pretty Good"
+    updateData = {
+        "properties": {
+            "Value": {
+                "rich_text": [
+                    {
+                        "text": {
+                            "content": "Pretty Good"
+                        }
+                    }
+                ]
             }
-          }
-        ]
-      }
+        }
     }
-  }
 
-  data = json.dumps(updateData)
+    data = json.dumps(updateData)
 
-  response = requests.request("PATCH", updateUrl, headers=headers, data=data)
+    response = requests.request("PATCH", updateUrl, headers=headers, data=data)
 
-# readDatabase(databaseId,headers)
-createPage(databaseId, headers)
+    print("Update Page Status Code: ", response.status_code)
+    print("Update Page Response Text: ", response.text)
+
+
+pageId = 'a9befb72-d5f6-46ba-b1d9-80eaa5836135'
+updatePage(pageId, headers)
